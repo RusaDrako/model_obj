d294b977fcaf782feec3543c5823d07b2e73383c
Petukhov Leonid <rusadrako@yandex.ru>
2021-04-13 14:34:19 +0300

    Добавлен объект свободных запросов

--- src/aliases.php
+++ src/aliases.php
@@ -7,6 +7,7 @@ if (class_exists('RD_Obj_Object', false)) {
 $classMap = [
 	'RusaDrako\\model_obj\\factory'       => 'RD_Obj_Factory',
 	'RusaDrako\\model_obj\\data'          => 'RD_Obj_Data',
+	'RusaDrako\\model_obj\\data_query'    => 'RD_Obj_Data_Query',
 	'RusaDrako\\model_obj\\object_list'   => 'RD_Obj_List',
 	'RusaDrako\\model_obj\\object_item'   => 'RD_Obj_Item',
 ];
